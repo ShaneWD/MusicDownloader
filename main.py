736f9d6cb45e04097230d80fe651e1d9f450a905
Shane <>
2021-06-08 22:33:16 -0400

    changed some variables for better code readability

--- main.py
+++ main.py
@@ -11,17 +11,17 @@ Provide the songs as previously stated (no apostrophes, or quotation marks!)
 song_count = 0
 for link in multi_names:
     os.system(cmd + link)
-    testing = subprocess.check_output(
+    raw_data = subprocess.check_output(
         ["youtube-dl", "-f", "bestaudio", "--extract-audio",
          "--audio-format", "mp3", "--audio-quality", "0", link],
         shell=True)
 
     song_count += 1
-    print("checked song #" + str(song_count))
+    print("CHECKED SONG #" + str(song_count))
 
-    testing = testing.decode('utf-8', errors="backslashreplace").split('\n')
+    data = raw_data.decode('utf-8', errors="backslashreplace").split('\n')
     file_name = []
-    for result in testing:
+    for result in data:
         if "Destination" in result:
             file_name.append(result.split(":")[1][1:-4])
 
