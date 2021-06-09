9f5a9f48a8a2004e67f81e568ce45ea85ac51af8
Shane <>
2021-06-09 13:25:29 -0400

    added useful comments

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import subprocess
 
-
+# command for downloading YouTube videos as MP3 with best audio quality
 cmd = "youtube-dl -f bestaudio --extract-audio --audio-format mp3 --audio-quality 0 "
 
 multi_names = input("""
@@ -11,6 +11,8 @@ Provide the songs as previously stated (no apostrophes, or quotation marks!)
 song_count = 0
 for link in multi_names:
     os.system(cmd + link)
+
+    # Record the listed information that it provides while running the download command
     raw_data = subprocess.check_output(
         ["youtube-dl", "-f", "bestaudio", "--extract-audio",
          "--audio-format", "mp3", "--audio-quality", "0", link],
@@ -18,19 +20,28 @@ for link in multi_names:
 
     song_count += 1
 
+    # Break the raw data at every new line
     data = raw_data.decode('utf-8', errors="backslashreplace").split('\n')
     file_name = []
     for result in data:
+
+        # Add data follows the word "Destination"
         if "Destination" in result:
             file_name.append(result.split(":")[1][1:-4])
+            # removes the initial space & the "webm" text
+            # upload that information to a list
 
+    # Only need the first result
     file_name = file_name[0]
     file_name = file_name + "mp3"
 
+    # Remove the unneeded code from the filename
     new_name = file_name[0:-16]
     new_name = new_name + ".mp3"
 
     os.system(f"""rename "{file_name}" "{new_name}" """)
+
+    # Give useful information to the user
     print(
         f""" -> Old file name:"{file_name}"
  -> New file name: "{new_name}" """)
